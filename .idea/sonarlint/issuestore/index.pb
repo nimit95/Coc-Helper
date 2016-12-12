d8c37ed6be49d8d366b38fbf963add6996c1c4ed
dell <piyush6348@gmail.com>
2016-12-12 14:36:50 +0530

    feat: Architecture made, Butterknife added

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-ƒ
-Sapp\src\main\java\capstoneproject\androidnanodegree\com\cochelper\MainActivity.java,1\4\147b1e00d9ba02c86b2f7b92f2f47674a3663d10
--- app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/activity/MainActivity.java
+++ app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/activity/MainActivity.java
@@ -1,7 +1,10 @@
-package capstoneproject.androidnanodegree.com.cochelper;
+package capstoneproject.androidnanodegree.com.cochelper.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import capstoneproject.androidnanodegree.com.cochelper.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        // key=AIzaSyAsKOFTWUN1yRGJ3Bd0SoRWCzSNWkybFoU
     }
 }
