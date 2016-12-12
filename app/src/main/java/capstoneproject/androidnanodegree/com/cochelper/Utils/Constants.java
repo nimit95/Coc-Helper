352577bff078d1b44867ec51fcec68b652f8ec0f
Nimit Agg <nimitagg95@gmail.com>
2016-12-12 16:43:46 +0530

    feat: Add Utils, model and network

---
+++ app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/Utils/Constants.java
@@ -0,0 +1,15 @@
+package capstoneproject.androidnanodegree.com.cochelper.Utils;
+
+/**
+ * Created by Nimit Agg on 12-12-2016.
+ */
+public interface Constants {
+
+    int MAX_SEEK_VALUE = 89;
+
+    String EVENT_SERVICE = "service";
+    String TAG_SHARED_PREF = "sharedPreferences";
+
+
+
+}
---
+++ app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/models/Badge.java
@@ -0,0 +1,23 @@
+package capstoneproject.androidnanodegree.com.cochelper.models;
+
+/**
+ * Created by Nimit Agg on 12-12-2016.
+ */
+
+public class Badge {
+    private String id;
+    private String name;
+    private String[] iconUrls;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getIconUrls() {
+        return iconUrls;
+    }
+}
