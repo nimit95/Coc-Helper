b883a7e982d284fe991e333a826edc6c467234ab
Nimit Agg <nimitagg95@gmail.com>
2016-12-13 01:29:14 +0530

    feat: On click video

--- app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/Utils/Constants.java
+++ app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/Utils/Constants.java
@@ -11,4 +11,5 @@ public interface Constants {
     String TAG_SHARED_PREF = "sharedPreferences";
     String BASE_URL_PROFILE="https://api.clashofclans.com/v1/players/%23G2U9YL8L";
     String YOUTUBE_QUERY_URL="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=viewCount&q=clash of clans&type=video&key=AIzaSyAsKOFTWUN1yRGJ3Bd0SoRWCzSNWkybFoU";
+    String YOUTUBE_VIDEO_URL = "https://www.youtube.com/watch?v=";
 }
--- app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/adapter/VideoListCursorAdapter.java
+++ app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/adapter/VideoListCursorAdapter.java
@@ -4,7 +4,9 @@ package capstoneproject.androidnanodegree.com.cochelper.adapter;
  * Created by Nimit Agg on 12-12-2016.
  */
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,31 +19,47 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 import capstoneproject.androidnanodegree.com.cochelper.R;
+import capstoneproject.androidnanodegree.com.cochelper.database.QuoteProvider;
+import capstoneproject.androidnanodegree.com.cochelper.utils.Constants;
 
 
 public class VideoListCursorAdapter extends CursorRecyclerViewAdapter<VideoListCursorAdapter.ViewHolder>{
 
-    private Context context;
+    static private Context context;
+    //public Cursor c;
     public VideoListCursorAdapter(Context context,Cursor cursor){
         super(context,cursor);
         this.context = context;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public TextView headline;
         public ImageView image;
+
 
         public ViewHolder(View view) {
             super(view);
             headline = (TextView) view.findViewById(R.id.heading);
             image = (ImageView) view.findViewById(R.id.thumbnail);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Cursor c=view.getContext().getContentResolver().query(QuoteProvider.Quotes.CONTENT_URI,null,null,null,null);
+            c.moveToFirst();
+            c.moveToPosition(getAdapterPosition());
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.YOUTUBE_VIDEO_URL + c.getString(c.getColumnIndex("videoId")) ));
+            context.startActivity(intent);
         }
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)  {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.video_card, parent, false);
+
         ViewHolder vh = new ViewHolder(itemView);
 
         return vh;
@@ -55,5 +73,7 @@ public class VideoListCursorAdapter extends CursorRecyclerViewAdapter<VideoListC
                 .load(cursor.getString(cursor.getColumnIndex("url")))
                 .into(viewHolder.image);
 
+        //c=cursor;
     }
+
 }
