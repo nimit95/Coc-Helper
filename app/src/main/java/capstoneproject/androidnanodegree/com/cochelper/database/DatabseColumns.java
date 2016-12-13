3f2912fba32667b5d99e5dab35fa9d31987aa1a7
Nimit Agg <nimitagg95@gmail.com>
2016-12-13 16:46:02 +0530

    feat: Loaders Added

--- app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/database/DatabseColumns.java
+++ app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/database/DatabseColumns.java
@@ -1,10 +1,14 @@
 package capstoneproject.androidnanodegree.com.cochelper.database;
 
+import net.simonvt.schematic.annotation.AutoIncrement;
 import net.simonvt.schematic.annotation.DataType;
 import net.simonvt.schematic.annotation.NotNull;
 import net.simonvt.schematic.annotation.PrimaryKey;
 
 public class DatabseColumns {
+    @DataType(DataType.Type.INTEGER) @NotNull @PrimaryKey @AutoIncrement
+    public static final String ID="_id";
+
     @DataType(DataType.Type.TEXT) @NotNull
     public static final String VIDEOID = "videoId";
 
